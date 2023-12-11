5a10e5d1d1d524f18570bc5748ba77986b5e55d2
jhean <brizadaojhean@gmail.com>
2023-12-10 23:01:54 -0300

    :sparkles: feat: as sessões de images da section-projects

---
+++ src/components/home/section-projects/section-images-three.column.tsx
@@ -0,0 +1,59 @@
+import ImgSalaCozinha from '@/components/assets/img-sala-cozinha.jpg'
+import ImgSalaLazer from '@/components/assets/img-sala-lazer.jpg'
+import { Button } from '@/components/ui/button'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
+import Image from 'next/image'
+import ImageContainer from './ImageContainer'
+import {
+  GridContainerColumn,
+  GridContainerTwoCols
+} from './grid-container-two-cols'
+
+function SectionImagesThreeColumn() {
+  return (
+    <>
+      <GridContainerTwoCols className="pb-3 shadow-lg shadow-zinc-200">
+        {/* Column 1 */}
+        <GridContainerColumn>
+          <ImageContainer>
+            <Image
+              src={ImgSalaCozinha}
+              alt="Imagem que mostra a sala e a cozinha da casa."
+              fill
+              sizes="100vw"
+              className="object-cover"
+            />
+          </ImageContainer>
+          <Button className="w-fit gap-1 font-sans" variant="ghost">
+            Bathroom Design 3D{' '}
+            <div className="flex">
+              <ArrowRightIcon />
+              <ArrowRightIcon />
+            </div>{' '}
+          </Button>
+        </GridContainerColumn>
+        {/* Column 2 */}
+        <GridContainerColumn>
+          <ImageContainer>
+            <Image
+              src={ImgSalaLazer}
+              alt="Imagem que mostra a sala e parte de lazer da casa."
+              fill
+              sizes="100vw"
+              className="object-cover"
+            />
+          </ImageContainer>
+          <Button className="w-fit gap-1 font-sans" variant="ghost">
+            Tiny House Design 3D{' '}
+            <div className="flex">
+              <ArrowRightIcon />
+              <ArrowRightIcon />
+            </div>{' '}
+          </Button>
+        </GridContainerColumn>
+      </GridContainerTwoCols>
+    </>
+  )
+}
+
+export default SectionImagesThreeColumn
