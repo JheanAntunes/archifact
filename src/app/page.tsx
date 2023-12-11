80e9e8fe5156dafcf3400c19654c77a6f52d2483
jhean <brizadaojhean@gmail.com>
2023-12-10 21:30:43 -0300

    import: section-projects na home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import SectionInitial from '@/components/home/section-initial'
+import SectionProjects from '@/components/home/section-projects/section-projects'
 import SectionQuality from '@/components/home/section-quality/section-quality'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="my-8 flex flex-col gap-8">
       <SectionInitial />
       <SectionQuality />
+      <SectionProjects />
     </main>
   )
 }
