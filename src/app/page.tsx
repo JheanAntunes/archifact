c99b80987930fc9c7749cd14a7e2bbc5fbc64aa6
jhean <brizadaojhean@gmail.com>
2023-12-11 22:46:06 -0300

    import: section-meet-our-team na home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SectionExpertise from '@/components/home/section-expertise/section-expertise'
 import SectionInitial from '@/components/home/section-initial'
 import SectionInternationallyImplemented from '@/components/home/section-internationally-implemented/section-internationally-implemented'
+import SectionMeetOurTeam from '@/components/home/section-meet-our-team/section-meet-our-team'
 import SectionProjects from '@/components/home/section-projects/section-projects'
 import SectionQuality from '@/components/home/section-quality/section-quality'
 
@@ -12,6 +13,7 @@ export default function Home() {
       <SectionProjects />
       <SectionExpertise />
       <SectionInternationallyImplemented />
+      <SectionMeetOurTeam />
     </main>
   )
 }
